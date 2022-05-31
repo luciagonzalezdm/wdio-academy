eebb44a678405a978005eda075b514161d3c090a
luciagonzalezdm <lucia.abstracta@hotmail.com>
2022-05-31 16:26:52 -0300

    add data sets in signup

--- data/accounts.js
+++ data/accounts.js
@@ -21,6 +21,20 @@ export const validAccount = {
       mobilePhone: "59898123456",
       returnMsg: "Welcome to your account. Here you can manage all of your personal information and orders."
     },
+    {
+      testSpecification: "create a new account without specifying gender",
+      gender: "",
+      firstName: "John",
+      lastName: "Doe",
+      password: "passwordOK",
+      companyName: "Abstracta",
+      address: "Street 19",
+      city: "Miami",
+      state: "Florida",
+      postalCode: "50000",
+      mobilePhone: "59898123456",
+      returnMsg: "Welcome to your account. Here you can manage all of your personal information and orders."
+    },
    
   ];
   export const signUpAccountsError = [
@@ -30,6 +44,20 @@ export const validAccount = {
       firstName: "John",
       lastName: "Doe",
       password: "1234",
+      companyName: "Abstracta",
+      address: "Street 40",
+      city: "Orlando",
+      state: "Florida",
+      postalCode: "50000",
+      mobilePhone: "534566",
+      returnMsg: "There is 1 error"
+    },
+    {
+      testSpecification: "show an error message if firstname and lastname fields are empty",
+      gender: "id_gender1",
+      firstName: "",
+      lastName: "",
+      password: "1234802780",
       companyName: "Abstracta",
       address: "Street 40",
       city: "Orlando",
