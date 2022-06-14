22aa7a47eec7a1e1c611b0a3f566e170b4ecb619
luciagonzalezdm <lucia.abstracta@hotmail.com>
2022-06-14 16:45:07 -0300

    add visual regression in wdio.conf file

--- wdio.conf.js
+++ wdio.conf.js
@@ -1,3 +1,4 @@
+var path = require('path');
 exports.config = {
     //
     // ====================
@@ -115,7 +116,25 @@ exports.config = {
     // Services take over a specific job you don't want to take care of. They enhance
     // your test setup with almost no effort. Unlike plugins, they don't add new
     // commands. Instead, they hook themselves up into the test process.
-    services: ['chromedriver'],
+    services: [
+        [
+            'image-comparison',
+            {
+              baselineFolder: path.join(process.cwd(), './visual-regression/baseline/'),
+              formatImageName: '{tag}-{logName}-{width}x{height}',
+              screenshotPath: path.join(process.cwd(), './visual-regression/'),
+              savePerInstance: true,
+              autoSaveBaseline: true,
+              blockOutStatusBar: true,
+              blockOutToolBar: true,
+              // más opciones...
+            },
+          ],
+        
+        
+        
+        'chromedriver'], 
+    
     
     // Framework you want to run your specs with.
     // The following are supported: Mocha, Jasmine, and Cucumber
